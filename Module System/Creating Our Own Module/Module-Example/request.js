569bce81b365c7354951987e4aa8d4bac755bca0
Shema N. Valentin <shemavalentin@gmail.com>
2024-08-11 12:25:55 +0200

    Different ways of exporting and importing modules is

--- Module System/Creating Our Own Module/Module-Example/request.js
+++ Module System/Creating Our Own Module/Module-Example/request.js
@@ -14,3 +14,32 @@ module.exports = {
   // here, we export functions as objects
   send, /// By using ES6 shorthand
 };
+
+// NODE ALSO PROVIDES OTHER WAY OF EXPORTING MODULES
+
+// We can do this
+
+// let's say we have another const
+module.exports.REQUEST_TIMEOUT = 500;
+function encrypt(data) {
+  return "encrypted data";
+}
+// We can add exports on the function we need to export like so
+
+module.exports.send = function send(url, data) {
+  const encryptedData = encrypt(data);
+  console.log(`sending ${encryptedData} to ${url}`);
+};
+
+// module.exports = {
+//   REQUEST_TIMEOUT,
+//   send,
+// };
+
+/* 
+NOTE THAT WE CAN ALSO USE THE NODE SHORTHAND AND REMOVE module in front.
+
+the recommendation is to use the first method and it is clear now what your
+interface to your module would be and how it will be accessed rather than
+going into the whole file and find references
+*/
