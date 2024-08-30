e03a566181255407636d9db11f6bef4f2ef96f61
Shema N. Valentin <shemavalentin@gmail.com>
2024-08-30 10:52:11 +0200

    Real life blocking functions

--- ImprovingNodePerformance/server.js
+++ ImprovingNodePerformance/server.js
@@ -12,12 +12,32 @@ function delay(duration) {
   while (Date.now() - startTime < duration) {
     // while we are in our loop, the event loop in completely blocked
     // Then when we call the delay function down in the second route?
+    // NOTE THAT THIS WHILE LOOP WILL CAUSE THE SERVER TO SLOW DOWN.
   }
 }
+/*
+*  What are real life functions that act like the above WHILE loop, that is blocking the event loop?
+
+* TWO FUNCTIONS LIKELY TO SEE ARE:  
+ JSON.stringfy() : This function takes in a javascript object and returns the string representation of that object.
+ and
+ JSON.parse():  this function does the opposite of JSON.stringfy. It takes s stringfied object and returns the 
+ corresponding object
+
+ These functions mentioned don't take too long like while loop but they delay. 
+ No other request can be processed before this functions end what they are doing
+
+ Some other examples that block the event loop are like sorting the array.
+ for very large arrays this is going to block
+ [5, 1,2,3,4].sort()
+
+*/
 
 // Let's have two routes
 
 app.get("/", (req, res) => {
+  // JSON.stringify({}) => "{}"
+  // JSON.parse("{}") => {}
   res.send("Performance example");
 });
 
@@ -35,3 +55,14 @@ app.listen(PORT, () => {
 
 // Here was writting a blocking function to show how we can and where we can emprove the
 // server performance.
+
+const obj1 = {
+  name: "Valentin",
+  lastNane: "Shema",
+};
+
+JSON.stringify(obj1); // {"name": "Valentin","lastNane": "Shema'"}
+
+// NOW
+const jsonObj = JSON.stringify(obj1);
+JSON.parse(jsonObj);
